7d92cacccc04b567ad2df5db52e2726d6f2a2646
Ayabonga <nompetsheniayabonga@gmail.com>
2018-03-27 12:04:13 +0200

    Update regCheck.test.js

--- test/regCheck.test.js
+++ test/regCheck.test.js
@@ -1,10 +1,10 @@
 describe('regCheck function', function(){
 
     it('should return registration Code', function(){
-        assert.equal(regCheck('CY123'),false);
+        assert.equal(regCheck('DV 23 NB GP', 'GP'),true);
     });
     it('should not return registration Code', function(){
 
-        assert.equal(regCheck('CCZTYTRX'),false);
+        assert.equal(regCheck('DV 23 LP GP', 'MP'),false);
     });
 });
