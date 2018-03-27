f2d5e9a02506d1cc2716f58755ad3ec4be98e16d
Ayabonga <nompetsheniayabonga@gmail.com>
2018-03-27 12:09:56 +0200

    Update finditemsOver20.test.js

--- test/finditemsOver20.test.js
+++ test/finditemsOver20.test.js
@@ -9,13 +9,17 @@ describe('The findItemOver20 function' , function(){
     ],20)[{name : 'pears', qty : 37},
     {name : 'bananas', qty : 27}]);
     });
-    it('should Return items that are over 10 ', function(){
+    it('should Return items that are over 20 ', function(){
       assert.equal(findItemsOver20(
       [
           {name : 'apples', qty : 10},
-          {name : 'pears', qty : 7},
-          {name : 'bananas', qty : 17},
-          {name : 'apples', qty : 8}
-      ],10)[{name : 'bananas', qty : 17}]);
+          {name : 'pears', qty : 127},
+          {name : 'bananas', qty : 97},
+          {name : 'apples', qty : 81},
+          {name : 'lemon', qty : 11},
+      ],20)[{name : 'pears', qty : 127},
+      {name : 'bananas', qty : 97},
+      {name : 'apples', qty : 81}]);
     });
   });
+
