7dcc025e5808f9aa797945e80af604a9e8f063f3
Ayabonga <nompetsheniayabonga@gmail.com>
2018-03-27 12:38:50 +0200

    Update fromWhere.js

--- fromWhere.js
+++ fromWhere.js
@@ -1,15 +1,15 @@
 function fromWhere(registration){
 
-  if(registration==='CY'){
+  if(registration==='CY 1098'){
     return 'Bellville';
   }
 
-    else if(registration==='CJ'){
+    else if(registration==='CJ 583'){
       return 'Paarl';
       console.log(registration)
     }
 
-  else if(registration==='CA'){
+  else if(registration==='CA 464'){
   return 'Cape Town';
   }
 
