878d0911e60739a5afb41a4e9a27df6c0cfc3be7
Ayabonga <nompetsheniayabonga@gmail.com>
2018-03-27 12:33:29 +0200

    Update fromWhere.test.js

--- test/fromWhere.test.js
+++ test/fromWhere.test.js
@@ -1,13 +1,15 @@
 describe('fromWhere function', function(){
 
-    it('returns Paarl if a registration number starts with CJ otherwise returns some place.', function(){
-        assert.equal(fromWhere('CJ'),'Paarl');
-    });
+  it('returns Paarl if a registration number starts with CJ otherwise returns some place.', function(){
+      assert.equal(fromWhere('CJ 583'),'Paarl');
+  });
 
     it('returns Cape Town if a registration number starts with CA otherwise returns some place.', function(){
-        assert.equal(fromWhere('CA'),'Cape Town');
+        assert.equal(fromWhere('CA 464'),'Cape Town');
     });
-
+    it('returns Bellville if a registration number starts with CY otherwise returns some place.', function(){
+        assert.equal(fromWhere('CY 1098'),'Bellville');
+    });
     it('returns some other place if the registration number doesnt start with CA,CJ or CY.', function(){
         assert.equal(fromWhere('Some other place!'),'Some other place!');
     });
