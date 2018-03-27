e4911d9bb7a57bfbe02e79da5635944713f03afb
Ayabonga <nompetsheniayabonga@gmail.com>
2018-03-27 13:03:37 +0200

    Update countRegNumbers.tests.js

--- test/countRegNumbers.tests.js
+++ test/countRegNumbers.tests.js
@@ -1,11 +1,12 @@
-
 describe('displayEachRegNumber function', function(){
 
     it('should display each registration number in the list on a new line', function(){
-        assert.equal(countRegNumbers('CJ, CL, CY, CA'),true);
-    });
-    it('should fail to display each registration number in the list on a new line', function(){
 
-        assert.equal(countRegNumbers('CA 345 673,'),false);
+        assert.equal(countRegNumbers("CA 345 673, CJ 2535, CL 37437,CL 447"),1);
+    });
+    it('should return Paarl to display on a new line', function(){
+
+        assert.equal(countRegNumbers('CJ 345 673, CJ 2535, CJ 37437,CL 447'),3);
     });
 });
+
