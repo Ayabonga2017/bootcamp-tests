abec072fcf1b7c865c8f96218fa15fc2962da7b8
Ayabonga <nompetsheniayabonga@gmail.com>
2018-03-27 13:06:31 +0200

    Update countRegNumbers.tests.js

--- test/countRegNumbers.tests.js
+++ test/countRegNumbers.tests.js
@@ -1,10 +1,10 @@
 describe('displayEachRegNumber function', function(){
 
-    it('should display 1 Paarl registration number in the list on a new line', function(){
+    it('should return 1 Paarl registration number in the list on a new line', function(){
 
         assert.equal(countRegNumbers("CA 345 673, CJ 2535, CL 37437,CL 447"),1);
     });
-    it('should display 3 Paarl registration number in the list on a new line', function(){
+    it('should return 3 Paarl registration number in the list on a new line', function(){
 
         assert.equal(countRegNumbers('CJ 345 673, CJ 2535, CJ 37437,CL 447'),3);
     });
