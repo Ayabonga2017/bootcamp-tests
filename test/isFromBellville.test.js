10a589e6bbfe99c7d8124a0ce4305a59b0b7f7e0
Ayabonga <nompetsheniayabonga@gmail.com>
2018-03-27 10:07:25 +0200

    Update isFromBellville.test.js

--- test/isFromBellville.test.js
+++ test/isFromBellville.test.js
@@ -3,7 +3,7 @@ describe('isFromBellvile function', function(){
     it('returns true if a registration number is for Bellville otherwise returns false.', function(){
         assert.equal(isFromBellville('CY123'),true);
     });
-    it('returns false if a registration number is for Bellville otherwise returns false.', function(){
+    it('returns false because this registration number is not from Bellville.', function(){
 
         assert.equal(isFromBellville('CJ123'),false);
     });
